2bec56d55b04f7be4ca35f912ca686174ad80c99
c25120ym <c25120ym@aiit.ac.jp>
2025-06-11 02:17:17 +0000

    actionを試す

--- test.sh
+++ test.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 #script.shのテスト
+tmp=/tmp/$$
 
-EXIT(){
+ERROR_EXIT(){
         echo $1 1>&2
+        rm -f /tmp/3465993--*
         exit 1
 }
 
 #test1 入力が足りないとき
-ans="error: 引数が多い"
-result=$(./script.sh)
-[ $ans = $result ] && echo "test1-1 ok" || EXIT "test 1-1"
+echo "input 2 arguments" > $tmp-ans
+./script.sh 2> $tmp-result && ERROR_EXIT "1-1: execution error"
+diff $tmp-ans $tmp-result || ERROR_EXIT "1-1: message is diff" 
+echo "test1-1 passed"
 
-result=$(./script.sh 1)
-[ $ans = $result ] && echo "test1-1 ok" || EXIT "test 1-2"
+
+if [ "${ans}" = "${result}" ]; then
+echo "test1-1 ok"
+else
+EXIT "test1-1 ok"
+fi
 
 #入力が多いとき
-ans="error: 引数が多い"
+ans="erro: 引数が多い"
 result=$(./script.sh 1 2 3)
 
 #一致した場合
 ans="equal"
+
 #一致しなかった場合
 ans="not equal"
+
+rm -f /tmp/3465993--*
